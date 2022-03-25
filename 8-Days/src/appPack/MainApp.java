01638347edaf150b5da4920890930fbcf4e1d1c0
Hakan ÖZER <hakanozer02@gmail.com>
2022-03-25 09:07:54 +0300

    8-Days

---
+++ 8-Days/src/appPack/MainApp.java
@@ -0,0 +1,22 @@
+package appPack;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+        // Collections
+        // ArrayList
+        // Generic Types
+        Settings<String> set = new Settings<>();
+        set.action("Ali");
+
+        ArrayList<String> ls = new ArrayList<>();
+        List<String> lsx = new ArrayList<>();
+
+
+
+    }
+}
---
+++ 8-Days/src/appPack/Settings.java
@@ -0,0 +1,9 @@
+package appPack;
+
+public class Settings<T> {
+
+    public void action( T a ) {
+        System.out.println( "Value :" + a );
+    }
+
+}
