85f64c5fd0e24147e260d9520d9ced30ae60c797
Hakan ÖZER <hakanozer02@gmail.com>
2022-05-22 00:01:51 +0300

    Update PersonRestController.java

--- Spring/3-Days/src/main/java/com/example/days3/restcontrollers/PersonRestController.java
+++ Spring/3-Days/src/main/java/com/example/days3/restcontrollers/PersonRestController.java
@@ -2,6 +2,7 @@ package com.example.days3.restcontrollers;
 
 import com.example.days3.services.UtilService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.LinkedHashMap;
@@ -15,13 +16,20 @@ public class PersonRestController {
         this.uService = uService;
     }
 
-    @GetMapping("/sum")
-    public Map<String, Object> sum( int a, int b ) {
+    @GetMapping("/sum/{a}/{b}")
+    public Map<String, Object> sum( @PathVariable(name = "a") int ax, @PathVariable int b ) {
         Map<String, Object> hm = new LinkedHashMap<>();
-        int sm = uService.sum(a, b);
+        int sm = uService.sum(ax, b);
         hm.put("Sum", sm);
         return hm;
     }
 
 
 }
+
+// @RequestBody -> Json nesnesini entity formata serileştirme için
+// Validation api içerisindeki özellikleri kullanmak için @RequestBody ile data alımı önerilir.
+
+// @RequestParam -> Post yöntemi ile gelen form elemanlarının datasını yakalamk için kullanılır.
+// @PathVariable -> Get yöntemi ile url bölümünde valueları "/" ile ayırarak almak istediğimizde kullanacağımız yöntemdir.
+// QueryString -> = ? işareti ile key=val yöntemi ile veri alımı için kullanılır. Herhangi bir notasyona gerek yoktur.
