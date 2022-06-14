1d59aa40a740ea88047baf7db042f5b4247f7d6a
Hakan ÖZER <hakanozer02@gmail.com>
2022-06-14 23:51:56 +0300

    React - One Days

---
+++ React/onedays/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <>
+      <div className='row'>
+        <div className='col-sm-4'><h1>Left</h1></div>
+        <div className='col-sm-4'><h1>Center</h1></div>
+        <div className='col-sm-4'><h1>Right</h1></div>
+      </div>
+    </>
+  )
+}
+
+export default App;
